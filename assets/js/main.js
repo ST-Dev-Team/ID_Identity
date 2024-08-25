a589bd846633c271578d2f6891a0501105e97de9
goldanghenry <wshdhkd@naver.com>
2024-08-26 01:33:30 +0900

    main page v1

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,7 @@
     const selectBody = document.querySelector('body');
     const selectHeader = document.querySelector('#header');
     if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
-    window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
+    window.scrollY > -1 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
   }
 
   document.addEventListener('scroll', toggleScrolled);
@@ -187,23 +187,23 @@
   /**
    * Navmenu Scrollspy
    */
-  let navmenulinks = document.querySelectorAll('.navmenu a');
+  // let navmenulinks = document.querySelectorAll('.navmenu a');
 
-  function navmenuScrollspy() {
-    navmenulinks.forEach(navmenulink => {
-      if (!navmenulink.hash) return;
-      let section = document.querySelector(navmenulink.hash);
-      if (!section) return;
-      let position = window.scrollY + 200;
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        document.querySelectorAll('.navmenu a.active').forEach(link => link.classList.remove('active'));
-        navmenulink.classList.add('active');
-      } else {
-        navmenulink.classList.remove('active');
-      }
-    })
-  }
-  window.addEventListener('load', navmenuScrollspy);
-  document.addEventListener('scroll', navmenuScrollspy);
+  // function navmenuScrollspy() {
+  //   navmenulinks.forEach(navmenulink => {
+  //     if (!navmenulink.hash) return;
+  //     let section = document.querySelector(navmenulink.hash);
+  //     if (!section) return;
+  //     let position = window.scrollY + 200;
+  //     if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+  //       document.querySelectorAll('.navmenu a.active').forEach(link => link.classList.remove('active'));
+  //       navmenulink.classList.add('active');
+  //     } else {
+  //       navmenulink.classList.remove('active');
+  //     }
+  //   })
+  // }
+  // window.addEventListener('load', navmenuScrollspy);
+  // document.addEventListener('scroll', navmenuScrollspy);
 
 })();
