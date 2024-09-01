c783a394c9fdf62fa97f7da8dd80719ce93de778
goldanghenry <wshdhkd@naver.com>
2024-09-01 23:46:44 +0900

    about - poster 추가

--- assets/js/main.js
+++ assets/js/main.js
@@ -185,6 +185,35 @@
   });
 
   /**
+   * Property carousel
+   */
+  new Swiper('#property-carousel', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.propery-carousel-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      }
+    }
+  });
+
+  /**
    * Navmenu Scrollspy
    */
   // let navmenulinks = document.querySelectorAll('.navmenu a');
